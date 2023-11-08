292ef8b640c62db37243d00a3a334633c177e3e0
sam <sampreethm@proton.me>
2023-11-08 07:41:39 +0530

    meow

--- signup.js
+++ signup.js
@@ -4,32 +4,61 @@ import {
     createUserWithEmailAndPassword,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
-var userId = null;
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 const auth = getAuth(app);
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        window.location.href = "home.html";
-    }
-    else {
-        //nothing to do
-    }
-});
+const database = getDatabase(app);
+// onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//         window.location.href = "home.html";
+//     }
+//     else {
+//         //nothing to do
+//     }
+// });
 
 var display = document.getElementById('result');
 const signUp = document.querySelector('#signup');
 signUp.addEventListener('click', createUser);
+
 function createUser() {
     const mail = document.getElementById('email');
     const email = mail.value;
     const pWord = document.getElementById('password');
     const password = pWord.value;
+    const username = document.getElementById("username").value;
+    const age = document.getElementById("age").value;
+    const city = document.getElementById("city").value;
+    const gG = document.getElementsByName('GENDER');
+    var gender = "";
+    for (let i = 0; i < gG.length; i++) {
+        if (gG[i].checked) {
+            gender = gG[i].value;
+        }
+    }
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
-            userId = user.uid;
+            const userId = user.uid;
             console.log(user);
-            window.location.href = `home.html`;
+            const profileObj = {
+                username: username,
+                age: age,
+                city: city,
+                gender: gender,
+                email: email
+            };
+            console.log(profileObj);
+            const reference = ref(database, 'users/' + userId);
+            console.log(reference);
+            set(reference, profileObj)
+                .then(() => {
+                    console.log("Successfull");
+                    window.location.href = "home.html";
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         })
         .catch((error) => {
             const errorCode = error.code;
