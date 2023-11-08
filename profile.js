beaca025347713ac5acc9b394401dfe364af8d16
sam <aquaticcalf@proton.me>
2023-11-08 14:20:31 +0530

    meow

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 import app from "./firebaseconfig.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
 const auth = getAuth(app);
 const db = getDatabase(app);
 var userId = null;
@@ -36,4 +36,37 @@ logOut.addEventListener('click', () => {
     }).catch((error) => {
         console.log(error);
     });
+});
+
+const edit = document.getElementById("done")
+edit.addEventListener('click', () => {
+    const username_edit = document.getElementById("username_edit").value
+    const age_edit = document.getElementById("age_edit").value
+    const city_edit = document.getElementById("city_edit").value
+    const email_edit = document.getElementById("email_edit").value
+    const gG = document.getElementsByName('GENDER_EDIT');
+    var gender_edit = "";
+    for (let i = 0; i < gG.length; i++) {
+        if (gG[i].checked) {
+            gender_edit = gG[i].value;
+        }
+    }
+const profileObj = {
+    username: username_edit,
+    age: age_edit,
+    city: city_edit,
+    gender: gender_edit,
+    email: email_edit
+};
+console.log(profileObj);
+const reference = ref(db, 'users/' + userId);
+console.log(reference);
+set(reference, profileObj)
+    .then(() => {
+        console.log("Successfull");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
 });
