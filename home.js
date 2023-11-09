2f8b47d041cf05c9d79b9623c6ec31488a1261f7
sam <aquaticcalf@proton.me>
2023-11-09 22:20:43 +0530

    meow

--- home.js
+++ home.js
@@ -27,21 +27,26 @@ onValue(ref(database, '/cityImgs'), (snapshot) => {
     const cityimgs = snapshot.val() || {};
     for (const city in cityimgs) {
         if (cityimgs.hasOwnProperty(city)) {
-          const cityimg = document.createElement('img');
-          cityimg.src = cityimgs[city];
-          cityimg.classList.add("rounded-xl")
-          cityimg.classList.add("grow")
       
           const holdmetight = document.getElementById('imghold');
           const cityname = document.createElement('p');
           cityname.textContent = city;
           cityname.classList.add("text-center")
+          cityname.classList.add("text-[#f9ac40]")
+          cityname.classList.add("text-[10vw]")
+          cityname.classList.add("lg:text-[100px]")
+          cityname.style.textStroke = '2px black';  
+          cityname.style.WebkitTextStroke = '2px black';
       
           const citygallery = document.createElement('div');
-          citygallery.appendChild(cityimg);
+          citygallery.style.backgroundImage = "url("+cityimgs[city]+")";
+          citygallery.style.backgroundSize = "cover"
           citygallery.appendChild(cityname);
           citygallery.classList.add("p-2")
-          citygallery.classList.add("bg-[#f9ac40]")
+          citygallery.classList.add("flex")
+          citygallery.classList.add("justify-center")
+          citygallery.classList.add("items-center")
+          citygallery.classList.add("h-[200px]")
           citygallery.classList.add("rounded-xl")
           citygallery.classList.add("flex")
           citygallery.classList.add("flex-col")
